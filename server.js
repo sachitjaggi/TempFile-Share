25dde78aac4941668b46a33d58d4fd522afd3142
sachitjaggi <sachitjaggi.1562@gmail.com>
2021-07-01 18:55:30 +0530

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const path=require('path');
 
 const app =express();
-const port= process.env.port || 5000 ; 
+const port=  process.env.PORT || 5000 ; 
 
 app.use(express.static('public'));
 
@@ -22,9 +22,7 @@ app.use('/files' ,require('./Routes/show'))
 app.use('/files/download',require('./Routes/download'));
 app.get('/',(req,res)=>{
     console.log(process.env.port);
-    res.json({
-        status:"Request recieved"
-    });
+    res.status(200).json({status:"Request recieved"});
     
 })
 
