64ef420fea0fd291a9dbd98daa45a751aa936955
sachitjaggi <sachitjaggi.1562@gmail.com>
2021-07-01 18:36:59 +0530

    homepage

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const path=require('path');
 
 const app =express();
-const port= process.env.PORT || 5000 ; 
+const port= process.env.port || 5000 ; 
 
 app.use(express.static('public'));
 
@@ -20,6 +20,13 @@ app.use(express.json());
 app.use('/api/files',require('./Routes/files'));
 app.use('/files' ,require('./Routes/show'))
 app.use('/files/download',require('./Routes/download'));
+app.get('/',(req,res)=>{
+    console.log(process.env.port);
+    res.json({
+        status:"Request recieved"
+    });
+    
+})
 
 app.listen(port,() =>{
     console.log(`Server is listening at port ${port} ..`);
